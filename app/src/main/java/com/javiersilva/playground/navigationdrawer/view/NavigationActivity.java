acd4f9ca03cea4fc14adec3286ed5016daa8ed98
Javier Silva <javier.sp1209@gmail.com>
2017-05-08 23:52:24 +0200

    Navigate to collapsing toolbar

--- app/src/main/java/com/javiersilva/playground/navigationdrawer/view/NavigationActivity.java
+++ app/src/main/java/com/javiersilva/playground/navigationdrawer/view/NavigationActivity.java
@@ -1,19 +1,20 @@
 package com.javiersilva.playground.navigationdrawer.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.javiersilva.playground.R;
+import com.javiersilva.playground.collapsingtoolbar.view.CollapsingToolbarActivity;
 
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -60,9 +61,9 @@ public class NavigationActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_collapsing_toolbar) {
+            startActivity(new Intent(this, CollapsingToolbarActivity.class));
+        } else if (id == R.id.nav_kiosk) {
 
         } else if (id == R.id.nav_share) {
 
